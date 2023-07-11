810428c7316a00701fed46beb586e3e806783904
Matheus <matheus.rj25@hotmail.com>
2023-07-10 21:35:35 -0300

    update poo/conta.py

--- Alura/poo/conta.py
+++ Alura/poo/conta.py
@@ -2,7 +2,23 @@ def criar_conta(numero,titular,saldo,limite):
     conta = {"numero":numero,"titular":titular,"saldo":saldo,"limite":limite}
     return conta
 
+def deposita(conta,valor):
+    conta["saldo"] += valor
+
+def saca(conta,valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
 
 conta = criar_conta(123,"Matheus",55,1000)
 
-print(conta["titular"])
+print("Primeiro",conta)
+
+deposita(conta,500)
+print("Segundo",conta)
+
+saca(conta,100)
+print("Terceiro",conta)
+
+extrato(conta)
