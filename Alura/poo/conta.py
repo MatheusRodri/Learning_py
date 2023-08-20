79f2707e385cac676ab11721e362fa9f39d5ddc1
Matheus <matheus.rj25@hotmail.com>
2023-08-20 16:58:55 -0300

    update in conta.py

--- Alura/poo/conta.py
+++ Alura/poo/conta.py
@@ -5,6 +5,7 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+        self.__codigo_banco = "033"
 
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo,self.__titular))
@@ -12,8 +13,15 @@ class Conta:
     def deposita(self,valor):
         self.__saldo += valor
 
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
     def saca(self,valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("Saldo insuficiente")
     
     def transfere(self,valor,destino):
         self.saca(valor)
@@ -30,6 +38,30 @@ class Conta:
 
     def set_limite(self,limite):
         self.__limite = limite
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco(self):
+        return self.__codigo_banco
+    
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001','Caixa':'104','Bradesco':'237'}
 
 # Teste da classe
 
